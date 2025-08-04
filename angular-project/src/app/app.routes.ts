1cf57279223cfac6d564e1589e22b2388cfcfcf4
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-08-04 12:57:23 +0300

    add register component plus register route

--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
 import { Home } from './feature/home/home';
+import { Register } from './feature/register/register';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,9 @@ export const routes: Routes = [
     {
         path:'home',
         component:Home
+    },
+    {
+        path:'register',
+        component: Register
     }
 ];
