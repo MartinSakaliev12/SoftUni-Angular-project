5135231abff47ce88a24524e68ef3486e31357c5
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-08-05 13:57:35 +0300

    Create register form group

--- angular-project/src/app/feature/register/register.ts
+++ angular-project/src/app/feature/register/register.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  formBuilder = inject(FormBuilder)
+  registerForm: FormGroup;
+  constructor(){
+    this.registerForm = this.formBuilder.group({
+      username:'',
+      email:'',
+      password:'',
+      rePassword:'',
+      biography:'',
+      
+    })
+  }
 
+  register():void{
+    console.log('yessss')
+  }
 }
