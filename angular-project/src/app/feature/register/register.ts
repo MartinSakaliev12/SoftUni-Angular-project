db4f4e68cd53f5b8496bb3d2db29f45f27ea7b41
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-08-06 00:01:41 +0300

    Add error message for username

--- angular-project/src/app/feature/register/register.ts
+++ angular-project/src/app/feature/register/register.ts
@@ -11,68 +11,76 @@ import { RouterModule } from '@angular/router';
 export class Register {
   formBuilder = inject(FormBuilder)
   registerForm: FormGroup;
- 
-  constructor(){
+
+  constructor() {
     this.registerForm = this.formBuilder.group({
-      username:['',[Validators.required,Validators.minLength(4)]],
-      email:['',[Validators.required,Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)]],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)]],
       passwords: this.formBuilder.group({
-        password:['',[Validators.required,Validators.minLength(6)]],
-        rePassword:['',[Validators.required]],
-      },{validators:this.isPassowrdsMatch}),
-      biography:[''] 
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        rePassword: ['', [Validators.required]],
+      }, { validators: this.isPassowrdsMatch }),
+      biography: ['']
     })
   }
-  get username (){
+  get username() {
     return this.registerForm.get('username')
   }
-  get email (){
+  get email() {
     return this.registerForm.get('email')
   }
-  get passwords (){
+  get passwords() {
     return this.registerForm.get('passwords') as FormGroup
   }
-  
-  get isUsernameInvalid(){
-    if((this.username?.touched || this.username?.dirty) && (this.username.invalid || this.username.errors?.['minlength'])){
+
+  get isUsernameInvalid() {
+    if ((this.username?.touched || this.username?.dirty) && (this.username.invalid || this.username.errors?.['minlength'])) {
       return true
     }
     return false
   }
 
-  get isEmailInvalid (){
-    if((this.email?.touched || this.email?.dirty) && (this.email.invalid || this.email.errors?.['pattern'])){
+  get isEmailInvalid() {
+    if ((this.email?.touched || this.email?.dirty) && (this.email.invalid || this.email.errors?.['pattern'])) {
       return true
     }
     return false
   }
 
-  get isPasswordInvalid (){
-    if((this.passwords.get('password')?.touched || this.passwords.get('password')?.dirty) && (this.passwords.get('password')?.invalid || this.passwords.get('passwords')?.errors?.['minlength'])){
+  get isPasswordInvalid() {
+    if ((this.passwords.get('password')?.touched || this.passwords.get('password')?.dirty) && (this.passwords.get('password')?.invalid || this.passwords.get('passwords')?.errors?.['minlength'])) {
       return true
     }
     return false
   }
 
-  get isRePasswordInvalid(){
-    if(this.passwords.errors?.['passwordsMissmatch']){
+  get isRePasswordInvalid() {
+    if (this.passwords.errors?.['passwordsMissmatch']) {
       return true
     }
     return false
   }
 
-  // get usernameErrorMessage(){
-  //   //todo errors messages and add returning type of get
-  // }
-  register():void{
+  get usernameErrorMessage() {
+    if (this.username?.dirty || this.username?.errors?.['minlength']) {
+      return "Username have to be at least 4 characters."
+    }
+    if (this.username?.touched) {
+      return "Username is reqired!"
+    }
+
+    return ""
+    //todo errors messages and add returning type of get
+  }
+  register(): void {
     console.log(this.registerForm.get('passwords'))
   }
 
-  isPassowrdsMatch(passwords:FormGroup){
+  isPassowrdsMatch(passwords: FormGroup) {
     const password = passwords.get('password')
     const rePasssword = passwords.get('rePassword')
-    if(password && rePasssword && password.value !== rePasssword.value){
-      return {passwordsMissmatch: true}
+    if (password && rePasssword && password.value !== rePasssword.value) {
+      return { passwordsMissmatch: true }
     }
     return null
   }
