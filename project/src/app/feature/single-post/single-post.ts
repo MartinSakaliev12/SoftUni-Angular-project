e9c63ad9c31f4ce1bf74ef5f39befa14a14fa9c3
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-08-07 18:29:49 +0300

    Add title and router link to single-post component

--- project/src/app/feature/single-post/single-post.ts
+++ project/src/app/feature/single-post/single-post.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../../shared/models/post.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-single-post',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './single-post.html',
   styleUrl: './single-post.css'
 })
