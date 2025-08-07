85db185276a011015cc1d6da43654319e21e3cd1
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-08-07 18:01:28 +0300

    Can not add icons to components

--- project/src/app/feature/home/home.ts
+++ project/src/app/feature/home/home.ts
@@ -3,10 +3,12 @@ import { PostService } from '../../core/services/post.service';
 import { Post } from '../../shared/models/post.model';
 import {  CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
