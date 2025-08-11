b426a778cfd7b0013ff195a354fda2437801e613
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-08-11 13:13:21 +0300

    Solve problem with not refreshing component

--- project/src/app/feature/profile/profile.ts
+++ project/src/app/feature/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ProfileService } from '../../core/services/profile.service';
 import { Observable } from 'rxjs';
 import { User } from '../../shared/models/user.model';
@@ -11,13 +11,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
-export class Profile {
+export class Profile implements OnInit{
   private profileService = inject(ProfileService)
   private route = inject(ActivatedRoute)
 
   profile$:Observable<User>
 
+  userId:string = ''
+
   constructor(){
     this.profile$ = this.profileService.getProfile(this.route.snapshot.paramMap.get('userId'))
   }
+    ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.userId = params['id'];
+      this.loadProfile(this.userId); // тук зареждаш новите данни
+    });
+  }
+    loadProfile(id: string) {
+     this.profile$ = this.profileService.getProfile(this.route.snapshot.paramMap.get('userId'))
+    }
+  
 }
